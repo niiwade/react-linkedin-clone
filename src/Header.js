a5fe36d5c7b8cfe53f0633db6712c08249610f06
niiwade <niilantewade77@gmail.com>
2021-09-08 23:22:59 +0000

    push

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../src/styles/Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+
+            <div className="header_left">
+                <img src='' alt='' />
+                
+                <div className="header_field">
+                    <input type="text" placeholder="Search" />
+                </div>
+             </div>
+
+            <div className="header_right">
+
+            </div>
+        </div>
+    )
+} 
+
+export default Header
